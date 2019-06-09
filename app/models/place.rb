5ebb470ed25309f44b1aa7b8dd87e35bdb558038
Brett <brett.simington@gmail.com>
2019-06-09 19:46:29 +0000

    add comments in db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: true, length: { minimum: 3, too_long: "%{count} characters is the minimum allowed!" }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_185132) do
+ActiveRecord::Schema.define(version: 2019_06_09_193405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
